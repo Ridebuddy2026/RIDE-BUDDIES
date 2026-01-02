206d4fb061ec889d4dd2c28a83a47f16883b227a
Ridebuddy2026 <damanbelike04@gmail.com>
2026-01-02 15:05:38 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,22 @@
+import ReactDOM from 'react-dom/client';
+import { InternetIdentityProvider } from './hooks/useInternetIdentity';
+import { initEditor } from './hooks/useEditor';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import App from './App';
+import './index.css';
 
-import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
-import App from './app'
-import './index.css'
+const queryClient = new QueryClient();
+
+if (document.readyState === 'loading') {
+    document.addEventListener('DOMContentLoaded', () => initEditor());
+} else {
+    initEditor();
+}
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
-)
+    <QueryClientProvider client={queryClient}>
+        <InternetIdentityProvider>
+            <App />
+        </InternetIdentityProvider>
+    </QueryClientProvider>
+);
